ec2b2412abd34709c374c9c82761cac9ce7f541b
Ilyua <old.flanigan@gmail.com>
2017-07-02 19:15:04 +0300

    clicl size

--- adidas_site_parser.py
+++ adidas_site_parser.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 from lxml import etree,html
 import requests
+import time
+from selenium import webdriver
 #response = requests.get("http://www.adidas.ru/muzhchiny-krossovki")
 doc='./adidas_all.html'#response.content
 
@@ -19,3 +21,19 @@ def get_links_of_goods(path=None,url=None):
 pprint(get_links_of_goods(path = doc))
 
 
+driver = webdriver.Firefox()
+driver.get(get_links_of_goods(path = doc)[0])
+
+actions = webdriver.common.action_chains.ActionChains(driver)
+actions.move_by_offset(100, 100).perform()
+time.sleep(1)
+actions.click().perform()
+
+element = driver.find_element_by_xpath('//div[@class="size-dropdown-block"]')
+actions.move_to_element_with_offset(element, 100,10)
+actions.click()
+actions.perform()
+
+# element = driver.find_element_by_xpath('//button[@name="add-to-cart-button"]')
+# element.click()
+# driver.quit()
