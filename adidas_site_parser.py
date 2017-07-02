2cf290c723153fa29d1630bb0b8a03ea624bcbfa
Ilyua <old.flanigan@gmail.com>
2017-07-02 17:19:29 +0300

    got links of goods

--- adidas_site_parser.py
+++ adidas_site_parser.py
@@ -1,15 +1,21 @@
 from pprint import pprint
-from lxml import etree
-doc = './adidas_all.html'
-f =open(doc,'r')
-doc = f.read()
-f.close()
-tree = etree.HTML(doc)
-result = etree.tostring(tree, pretty_print=True, method="html")
-#print(result)
-r = tree.xpath('//div[@class="product-info-inner-content clearfix with-badges"]')
-pprint(r)
-for i in r:
-    pprint(etree.tostring(i, pretty_print=True,method="html").decode())
+from lxml import etree,html
+import requests
+#response = requests.get("http://www.adidas.ru/muzhchiny-krossovki")
+doc='./adidas_all.html'#response.content
+
+def get_links_of_goods(path=None,url=None):
+    if (path is None) and (url is not None):
+        response = requests.get(url)
+        doc = response.content
+    elif (path is not None) and (url is None):
+        with open(path,'r') as f:
+            doc = f.read()
+    else:
+        print('error')
+    tree = etree.HTML(doc)
+    goods = tree.xpath('//div[@class="product-info-inner-content clearfix with-badges"]//a')
+    return [item.get('href') for item in goods]
+pprint(get_links_of_goods(path = doc))
 
 
